9445c51fdfbe2fb19ae1185f9a60b4618d9c8b05
Jesse Copeland <jesse.copeland@gmail.com>
2015-01-30 12:01:16 -1000

    implemented invoke method, tests pass

--- constructors.js
+++ constructors.js
@@ -156,3 +156,24 @@ Spellcaster.prototype.spendMana = function (cost) {
    * @param  {Spellcaster} target         The spell target to be inflicted.
    * @return {boolean}                    Whether the spell was successfully cast.
    */
+
+Spellcaster.prototype.invoke = function (spell, target) {
+  if (spell instanceof DamageSpell && target instanceof Spellcaster) {
+    if (this.spendMana(spell.cost)) {
+      target.inflictDamage(spell.damage);
+      return true;
+    } else {
+      return false;
+    }
+  } else if (spell instanceof DamageSpell && !(target instanceof Spellcaster)) {
+    return false;
+  } else if (spell instanceof Spell) {
+    if (this.spendMana(spell.cost)) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+};
